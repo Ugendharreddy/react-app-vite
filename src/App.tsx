c462cddfdfa722bc82dc4451ba847e3d2caf445d
Yougendher Enjam <yougendher.reddy-enjam@metlife.com>
2022-07-06 11:29:30 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "enjam-react-ts-boilerplate/lib"
+
 export default function App() {
     return (
       <>
         <div>Hello World</div>
+        <Button>{"Click Me!"}</Button>
       </>
     );
   }
