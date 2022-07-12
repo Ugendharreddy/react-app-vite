89dc0acd64f617bc660b4a1be64e2aa02463626a
Yougendher Enjam <yougendher.reddy-enjam@metlife.com>
2022-07-12 14:38:19 -0400

    update, change the dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "enjam-react-ts-boilerplate/lib"
+import { MyButton } from "enjam-vite-react-library"
 
 export default function App() {
     return (
       <>
         <div>Hello World</div>
-        <Button>{"Click Me!"}</Button>
+        <MyButton>{"Click Me!"}</MyButton>
       </>
     );
   }
